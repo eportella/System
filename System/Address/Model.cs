12487305a405a8deda847f47e4df67972483e981
Ewerton <portella.eweton@gmail.com>
2018-03-09 00:48:18 -0300

    Comentário sobre entidades fracas no endereço

--- System/Address/Model.cs
+++ System/Address/Model.cs
@@ -4,6 +4,7 @@
 
     public class Model
     {
+        /* na minha visão se endereço é uma entidade fraca eu simplemente não represento os donos aqui porque nunca vou precisar começar nada partindo de endereço */
         public IEnumerable<Owner.Interface> Owners { get; set; }
     }
 }
