17b6170afc614acce97efa23c707fa1671003e39
Ewerton da Silva Portella <portella.ewerton@gmail.com>
2018-03-09 00:57:19 -0300

    Update Model.cs

--- System/Client/Model.cs
+++ System/Client/Model.cs
@@ -1,8 +1,10 @@
 ﻿namespace System.Client
 {
     using System.Collections.Generic;
+     /* cliente é dono de um endereço */
     public class Model : Address.Owner.Interface
     {
+        /* cliente tem um endereço */
         public IEnumerable<Address.Model> Addresses { get; set; }
     }
 }
